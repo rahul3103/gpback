00f6db61c69aa74a7da34c757867a8f3f19d8dd9
Rahul Shrivastava <rahul3103@gmail.com>
2017-11-19 19:25:18 +0530

    settings updated

--- settings.py
+++ settings.py
@@ -3,10 +3,5 @@ DB_USER = 'rahul'
 DB_PASS = 'password'
 DB_HOST = 'localhost'
 
-WEB_URL = '//localhost:3000'
-
-client_id = '2117634981801814'
-client_secret = '1f38cd50e4671b2c82d0c827912d2cad'
-redirect_uri = 'http://localhost:5000/callback/facebook'
-authorization_base_url = 'https://www.facebook.com/dialog/oauth'
-token_url = 'https://graph.facebook.com/oauth/access_token'
+# WEB_URL = '//localhost:3000'
+WEB_URL = 'https://rahul3103.github.io/gpfront/'
